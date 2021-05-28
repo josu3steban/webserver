17e4c6bea3cb3c495110d36a83f27506d383fe50
Josue <josue_3steban@hotmail.com>
2021-05-28 14:28:46 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+app.use( express.static( __dirname + '/public' ));
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.get('/', ( req, res ) => {
+
+    res.render( 'home', {
+
+        nombre: 'Josue',
+        anio: new Date().getFullYear()
+        
+    });
+
+});
+
+ 
+app.listen(3000, () => {
+
+    console.log('Escuchando peticiones en el puerto 3000');
+    
+})
